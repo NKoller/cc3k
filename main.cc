f0893d36365a7c3066baf65975d13693e67401b6
j2999nes <j2999nes@student.cs.uwaterloo.ca>
2018-07-17 16:57:05 -0400

    new file:   main.cc
    	new file:   main.cc

---
+++ main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string s1, s2;
+	while (true){
+		cin >> s1;
+		if (s1 == "q"){
+			cout << "quit" << break;
+		}
+		if (s1 == "no"){
+			continue;
+		}			
+	}
+}
