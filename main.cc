5f0a16d090645c0d29ca7313a02c4d845b8edabe
JNesher <41345963+JNesher@users.noreply.github.com>
2018-07-17 17:24:42 -0400

    Add files via upload

--- main.cc
+++ main.cc
@@ -6,10 +6,59 @@ int main(){
 	while (true){
 		cin >> s1;
 		if (s1 == "q"){
-			cout << "quit" << break;
+			cout << "quit" << endl;
+            break;
 		}
 		if (s1 == "no"){
+            cout << s1 << endl;
 			continue;
-		}			
-	}
+		}	else if (s1 == "so"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "ea"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "we"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "ne"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "nw"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "se"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "sw"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "s"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "d"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "v"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "g"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "t"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "f"){
+            cout << s1 << endl;
+			continue;
+		}	else if (s1 == "r"){
+            cout << s1 << endl;
+        }   else if (s1 == "u"){
+            cin >> s2;
+            cout << s1 << " " << s2 << endl;
+        }   else if (s1 == "a"){
+            cin >> s2;
+            cout << s1 << " " << s2 << endl;
+        }
+    }
 }
